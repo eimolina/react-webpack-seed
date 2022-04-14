dadd4192fe3d39a9e67dc88d865e77b6f1bdeb31
Isaac Molina <izaac.everaldo@gmail.com>
2022-04-13 20:48:37 -0600

    commit inicial

---
+++ webpack.config.dev.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(m?js|jsx)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        watchFiles: ["src/**/*"], //observa los cambios en todos nuestros archivos y actualiza el navegador
+    }
+}
